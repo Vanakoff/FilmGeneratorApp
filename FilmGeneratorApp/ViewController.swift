0ef7f33ec2493ccb40319ab624a8fdec9d81cfee
Vanakoff <andrey.vanakoff@gmail.com>
2021-09-25 14:24:13 +0200

    Initial Commit

---
+++ FilmGeneratorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmGeneratorApp
+//
+//  Created by Andrey Vanakoff on 25/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
